a570e137beb8b3417371569007e55ef6ac35e2f4
nefigarcia <bluedivee@hotmail.com>
2016-08-29 17:02:13 -0500

    android2

---
+++ app/src/main/java/com/a16mb/rosystemsi/holanefi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a16mb.rosystemsi.holanefi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
